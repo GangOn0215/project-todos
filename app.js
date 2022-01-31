e8f9c92c219ba1ad265af607851228268bcfd6fe
GangOn0215 <ekdldktnekf@gmail.com>
2022-01-31 23:37:15 +0900

    connected mysql and testing

--- app.js
+++ app.js
@@ -2,15 +2,28 @@ const express = require("express");
 const hbs = require("express-handlebars");
 const app = express();
 const port = 3000;
+const mysql = require('mysql');
+let mysql_json_file = require('./private/database.json');
+mysql_json_file = JSON.parse(JSON.stringify(mysql_json_file));
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: mysql_json_file.id,
+  password: mysql_json_file.password,
+  database: 'test_account'
+});
+
+connection.connect();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.engine(
   "hbs",
   hbs.engine({
     extname: "hbs",
     defaultLayout: "layout",  //기본 레이아웃 설정
-    
+
     //레이아웃 디렉토리 설정
     layoutDir: __dirname + "/views/layouts"
   })
@@ -28,37 +41,48 @@ app.get('/', (req, res) => {
   res.status(200).render('index.hbs', {
     wiseSaying: '변명 중에서도 가장 어리석고 못난 변명은 "시간이 없어서" 라는 변명이다.',
   })
-  // res.sendFile(__dirname + "/public/index.html");
 });
 
-app.get('/account/login', (req,res) => {
-  // res.sendFile(__dirname + "/public/account/login.html");
+app.get('/account/login', (req, res) => {
   res.status(200).render('account/login.hbs', {})
 });
 
 app.get('/account/register', (req, res) => {
   res.status(200).render('account/register.hbs', {});
-  // res.sendFile(__dirname + "/public/account/register.html");
 });
 
-app.get('/study/email', (req,res) => {
+app.get('/study/email', (req, res) => {
   res.status(200).render('tutorial/email_form.hbs', {});
 })
 
 app.post('/post/email', (req, res) => {
-  // res.sendFile(__dirname + "/public/nodejs_tutorial/email_form.html");
   res.status(200).render('tutorial/email.hbs', {
     user_email: req.body.email
   });
 });
 
 app.post('/post/ajax_send_email', (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
+  const email = req.body.email;
   //validation: 확인
   // check validation aboout input value => select db
-  const responseData = {
-    'result': true, 
-    'email': req.body.email
+  let responseData = {
+    'result': false,
+    'nickname': 'none'
   }
-  res.json(responseData);
+  const query = connection.query(`select user_nickname from member where user_email="${email}"`, (err, rows) => {
+    if (err) throw err;
+    if (rows[0]) {
+      // console.log(rows[0].user_nickname);
+      responseData = {
+        'result': true,
+        'nickname': rows[0].user_nickname
+      }
+      console.log(1);
+    } else {
+      console.log(`none: ${rows[0]}`);
+    }
+
+    res.json(responseData);
+  });
 });
